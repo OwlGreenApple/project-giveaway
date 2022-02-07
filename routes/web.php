c8c897e77a2379c45c59147d7092a75ac50757bc
rizky redjo <celebgramme.dev@gmail.com>
2022-02-07 13:27:51 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,7 @@ Route::get('/list-broadcast', [App\Http\Controllers\BroadcastController::class, 
 Route::post('/save-broadcast', [App\Http\Controllers\BroadcastController::class, 'save_broadcast']);
 Route::post('/delete-broadcast', [App\Http\Controllers\BroadcastController::class, 'delete_broadcast']);
 Route::get('/edit-broadcast/{id}', [App\Http\Controllers\BroadcastController::class, 'edit_broadcast']);
+Route::get('/test-broadcast', [App\Http\Controllers\BroadcastController::class, 'test_broadcast']);
 
 
 /* Admin */
