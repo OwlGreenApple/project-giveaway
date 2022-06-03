721177cd27e46b95907153ee32f2a43aa44f8d17
gunomnifluencer <gunardi.omnifluencer@gmail.com>
2022-06-03 16:40:31 +0700

    fix-cron-check-event-duration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('check:running_events')->daily();
+        $schedule->command('check:running_events')->everyTenMinutes();
         $schedule->command('check:membership')->hourly();
         $schedule->command('check:purchase')->everySixHours();
         $schedule->command('check:membership_terms')->everyFifteenMinutes();
